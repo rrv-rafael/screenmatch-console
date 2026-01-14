d6cf4743dccfa2ed353d64d662c61495fcb68828
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2026-01-13 23:29:11 -0300

    implementação do retorno da lista de temporadas com a lista de episódios na classe principal

--- src/main/java/br/com/rrv/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/rrv/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,10 @@
 package br.com.rrv.screenmatch;
 
-import br.com.rrv.screenmatch.dtos.EpisodioDto;
-import br.com.rrv.screenmatch.dtos.SerieDto;
-import br.com.rrv.screenmatch.dtos.TemporadaDto;
-import br.com.rrv.screenmatch.services.ConsumoApi;
-import br.com.rrv.screenmatch.services.ConverteDados;
+import br.com.rrv.screenmatch.principal.Principal;
 import org.jspecify.annotations.NonNull;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -23,44 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String @NonNull ... args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Principal principal = new Principal();
 
-        var consumoApi = new ConsumoApi();
-
-        var apiKeyOmdb = "b8662a59";
-
-        var urlApi = "https://omdbapi.com/?t=gilmore+girls&apikey=" + apiKeyOmdb;
-        var jsonResponse = consumoApi.obterDados(urlApi);
-
-        ConverteDados converteDados = new ConverteDados();
-
-        SerieDto serieDto = converteDados.obterDados(jsonResponse, SerieDto.class);
-
-        System.out.println("Json response da série: " + jsonResponse);
-        System.out.println("DTO da série: " + serieDto);
-
-        urlApi = "https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=" + apiKeyOmdb;
-
-        jsonResponse = consumoApi.obterDados(urlApi);
-
-        EpisodioDto episodioDto = converteDados.obterDados(jsonResponse, EpisodioDto.class);
-
-        System.out.println("\nJson response do episódio: " + jsonResponse);
-        System.out.println("DTO do episódio: " + episodioDto);
-
-        List<TemporadaDto> temporadasDto = new ArrayList<>();
-
-        for (int i = 1; i <= serieDto.totalTemporadas(); i++) {
-            jsonResponse = consumoApi.obterDados(String.format("https://omdbapi.com/?t=gilmore+girls&season=%d&apikey=%s", i, apiKeyOmdb));
-
-            TemporadaDto temporadaDto = converteDados.obterDados(jsonResponse, TemporadaDto.class);
-
-            temporadasDto.add(temporadaDto);
-        }
-
-        System.out.println("\nLista de temporadas:");
-        temporadasDto.forEach(System.out::println);
-
-        scanner.close();
+        principal.exibirMenu();
     }
 }
--- src/main/java/br/com/rrv/screenmatch/principal/Principal.java
+++ src/main/java/br/com/rrv/screenmatch/principal/Principal.java
@@ -1,15 +1,18 @@
 package br.com.rrv.screenmatch.principal;
 
 import br.com.rrv.screenmatch.dtos.SerieDto;
+import br.com.rrv.screenmatch.dtos.TemporadaDto;
 import br.com.rrv.screenmatch.services.ConsumoApi;
 import br.com.rrv.screenmatch.services.ConverteDados;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
     private String urlApi;
     private final String URL_API = "https://omdbapi.com/?t=";
     private final String API_KEY = "&apikey=b8662a59";
@@ -28,7 +31,21 @@ public class Principal {
 
         SerieDto serieDto = converteDados.obterDados(jsonResponse, SerieDto.class);
 
-        System.out.println(serieDto);
+        System.out.println("Json response da série: " + jsonResponse);
+        System.out.println("DTO da série: " + serieDto);
+
+        List<TemporadaDto> temporadasDto = new ArrayList<>();
+
+        for (int i = 1; i <= serieDto.totalTemporadas(); i++) {
+            jsonResponse = consumoApi.obterDados(String.format("https://omdbapi.com/?t=%s&season=%d%s", nomeSerie, i, API_KEY));
+
+            TemporadaDto temporadaDto = converteDados.obterDados(jsonResponse, TemporadaDto.class);
+
+            temporadasDto.add(temporadaDto);
+        }
+
+        System.out.println("\nLista de temporadas:");
+        temporadasDto.forEach(System.out::println);
 
         scanner.close();
     }
